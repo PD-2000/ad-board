689fe813b65c9edaade12886f1abf2184896fc53
Pawel Domagala <domagalapawel11@gmail.com>
2023-08-08 22:47:43 +0200

    Fix indents in authMiddleware.js

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -1,9 +1,9 @@
 const authMiddleware = (req, res, next) => {
- if(req.session.login) {
-  next();
- } else {
-  res.status(401).send({message: "User unauthorized"});
- }
+  if(req.session.login) {
+    next();
+  } else {
+    res.status(401).send({message: "User unauthorized"});
+  }
 }
 
 module.exports = authMiddleware;
